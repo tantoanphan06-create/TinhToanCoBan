a3d1f5c536e849da2888d43f314a0d6533c2601f
tantoanphan06-create <tantoanphan06@gmail.com>
2026-01-11 22:36:26 +0700

    bài số 3

---
+++ PhanThanhTanToan.java
@@ -0,0 +1,17 @@
+package gioi_thieu;
+
+public class PhanThanhTanToan {
+    public static void main(String[] args) {
+        // Khai báo thông tin cá nhân
+        String hoTen = "Phan Thanh Tan Toan";
+        String maSoSV = "MSSV_CUA_BAN"; // Bạn thay mã số thật của mình vào đây nhé
+        String lop = "TEN_LOP_CUA_BAN"; // Bạn thay tên lớp thật vào đây nhé
+
+        // In thông tin ra màn hình
+        System.out.println("----- Phan Thanh Tan Toan -----");
+        System.out.println("Phan Thanh Tan Toan: " + hoTen);
+        System.out.println("1350070077: " + maSoSV);
+        System.out.println("Lớp:13-ĐH-HTTT1 " + lop);
+        System.out.println("-------------------------------");
+    }
+}
