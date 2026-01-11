cc831544f3aa89e7712e10076d7557c8f9f6b14a
tantoanphan06-create <tantoanphan06@gmail.com>
2026-01-11 22:42:28 +0700

    bài số 4

---
+++ HelloJava.java
@@ -0,0 +1,14 @@
+
+package bai_tap; 
+
+
+public class HelloJava {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Chao mung ban đen voi lap trinh Java!");
+        System.out.println("Cau truc Java that don gian!");
+    }
+}
